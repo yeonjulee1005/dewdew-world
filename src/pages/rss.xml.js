454cdd599255896a513a6acfc84894fa86d225b7
yeonjulee1005 <yeonju.lee1005@gmail.com>
2024-01-17 23:34:13 +0900

    🦕 develop version: 0.3.0 - `@vercel/analytics` 설정

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,19 +2,19 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-  const blog = await getCollection('blog')
+  const tech = await getCollection('tech')
   return rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
+    title: 'Dewdew Tech',
+    description: 'Dewdew Tech RSS Feed',
     site: context.site,
-    items: blog.map((post) => ({
+    items: tech.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       customData: post.data.customData,
       // 게시물의 `slug`에서 RSS 링크를 생성합니다
-      // 이 예에서는 모든 글이 `/blog/[slug]` 경로로 렌더링된다고 가정합니다.
-      link: `/blog/${post.slug}/`
+      // 이 예에서는 모든 글이 `/tech/[slug]` 경로로 렌더링된다고 가정합니다.
+      link: `/tech/${post.slug}/`
     }))
   })
 }
