9d0145bcd53f47c06c4fa2220318baa89b9eb92a
yeonjulee1005 <yeonju.lee1005@gmail.com>
2024-01-17 21:57:17 +0900

    🦕 develop version: 0.1.0 - tailwind 설정 - prettier 설정 - 기본 정적 Asset 구현 - 404 페이지 및 head 공통 설정 구현

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap()
+  ],
+  site: 'https://www.dewdew.world',
+  output: 'hybrid',
+  adapter: vercel({
+    edgeMiddleware: true
+  })
+});
