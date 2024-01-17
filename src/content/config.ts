d440677d5917b78bda92320617cf0329fcc9d13a
yeonjulee1005 <yeonju.lee1005@gmail.com>
2024-01-17 23:53:43 +0900

    🦕 develop version: 0.3.0 - `@vercel/analytics` 설정

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,23 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content'
+import { rssSchema } from '@astrojs/rss'
 
 // Define a schema for each collection you'd like to validate.
-const techCollection = defineCollection({
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.date(),
-      draft: z.boolean(),
-      cover: image(),
-      coverAlt: z.string(),
-    }),
+const tech = defineCollection({
+  type: 'content', // v2.5.0 이상
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    draft: z.boolean(),
+    cover: z.string().optional(),
+    coverAlt: z.string(),
+  })
+})
+
+const techRss = defineCollection({
+  schema: rssSchema
 })
 
 // Export a single `collections` object to register your collection(s)
-export const collections = {
-  tech: techCollection,
-}
+export const collections = { tech, techRss }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,19 +2,20 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-  const tech = await getCollection('tech')
-  return rss({
-    title: 'Dewdew Tech',
-    description: 'Dewdew Tech RSS Feed',
-    site: context.site,
-    items: tech.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: post.data.customData,
-      // 게시물의 `slug`에서 RSS 링크를 생성합니다
-      // 이 예에서는 모든 글이 `/tech/[slug]` 경로로 렌더링된다고 가정합니다.
-      link: `/tech/${post.slug}/`
-    }))
-  })
+  // const tech = await getCollection('techRss')
+  // console.log('tech', tech)
+  // return rss({
+  //   title: 'Dewdew Tech',
+  //   description: 'Dewdew Tech RSS Feed',
+  //   site: context.site,
+  //   items: tech.map((post) => ({
+  //     title: post.data.title,
+  //     pubDate: post.data.pubDate,
+  //     description: post.data.description,
+  //     customData: post.data.customData,
+  //     // 게시물의 `slug`에서 RSS 링크를 생성합니다
+  //     // 이 예에서는 모든 글이 `/tech/[slug]` 경로로 렌더링된다고 가정합니다.
+  //     link: `/tech/${post.slug}/`
+  //   }))
+  // })
 }
