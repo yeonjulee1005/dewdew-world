d1895b802a700d3770c489d5f228a0e189aaa91a
Yeonju Lee <yeonju.lee1005@gmail.com>
2024-01-22 16:58:33 +0900

    🦕 develop version: 0.6.0 - footer 생성중 - 모바일, 데스크탑 헤더 분리 - astro-icon 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import { markReadingTime } from './src/utils/readingTime.mjs'
 
 import tailwind from '@astrojs/tailwind'
+import icon from "astro-icon";
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel/serverless'
@@ -12,6 +13,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     }),
+    icon(),
     sitemap(),
     mdx()
   ],
