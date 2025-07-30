f969b068ba7aeeb186b0a4222909735c59474ce6
Yeonju Lee <yeonju.lee1005@gmail.com>
2025-07-31 08:19:45 +0900

    🦕 develop version: 1.25.0 - localization 적용 (영어,한글)으로 인한 전체 프로젝트 구조 변경 - github issue template 적용

---
+++ src/locales.ts
@@ -0,0 +1,35 @@
+// locales settings for this theme
+// Set the languages you want to support on your site.
+// https://astro-i18n-starter.pages.dev/setup/
+
+export const DEFAULT_LOCALE_SETTING: string = "en";
+
+interface LocaleSetting {
+	[key: Lowercase<string>]: {
+		label: string;
+		lang?: string;
+		dir?: "rtl" | "ltr";
+	};
+} // refer: https://starlight.astro.build/reference/configuration/#locales
+
+export const LOCALES_SETTING: LocaleSetting = {
+	en: {
+		label: "English",
+		lang: "en-US",
+	},
+  ko: {
+    label: "한국어",
+    lang: "ko-KR",
+  },
+	// ja: {
+	// 	label: "日本語",
+	// },
+	// "zh-cn": {
+	// 	label: "简体中文",
+	// 	lang: "zh-CN",
+	// },
+	// ar: {
+	// 	label: "العربية",
+	// 	dir: "rtl",
+	// },
+};
